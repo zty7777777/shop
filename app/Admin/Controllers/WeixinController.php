514d296f474544bdf30c46cca762fbabfcff7bd7
zty7777777 <724050978@qq.com>
2019-02-20 14:25:41 +0800

    储存用户

---
+++ app/Admin/Controllers/WeixinController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Model\WeixinUser;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class WeixinController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('Index')
+            ->description('description')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new WeixinUser);
+
+        $grid->id('Id');
+        $grid->uid('Uid');
+        $grid->openid('Openid');
+        $grid->add_time('Add time');
+        $grid->nickname('Nickname');
+        $grid->sex('Sex');
+        $grid->headimgurl('Headimgurl')->display(function ($lmg_url){
+            return '<img src="'.$lmg_url.'">';
+        });
+        $grid->subscribe_time('Subscribe time');
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(WeixinUser::findOrFail($id));
+
+        $show->id('Id');
+        $show->uid('Uid');
+        $show->openid('Openid');
+        $show->add_time('Add time');
+        $show->nickname('Nickname');
+        $show->sex('Sex');
+        $show->headimgurl('Headimgurl');
+        $show->subscribe_time('Subscribe time');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new WeixinUser);
+
+        $form->number('uid', 'Uid');
+        $form->text('openid', 'Openid');
+        $form->number('add_time', 'Add time');
+        $form->text('nickname', 'Nickname');
+        $form->number('sex', 'Sex');
+        $form->text('headimgurl', 'Headimgurl');
+        $form->number('subscribe_time', 'Subscribe time');
+
+        return $form;
+    }
+}
