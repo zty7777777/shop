eb828a317c37c3138b27a8c5ea1c9af779c36aac
zty7777777 <724050978@qq.com>
2019-02-21 10:07:30 +0800

    下载素材

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -86,30 +86,9 @@ class WeixinController extends Controller
                     var_dump($m_id);
                 }
             }elseif($xml->MsgType=='voice'){        //处理语音信息
-                $file_name= $this->dlVoice($xml->MediaId);
-
-                $xml_response = '<xml>
-                                        <ToUserName><![CDATA['.$openid.']]></ToUserName>
-                                        <FromUserName><![CDATA['.$xml->ToUserName.']]></FromUserName>
-                                        <CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType
-                                        ><Content><![CDATA['. date('Y-m-d H:i:s') .']]></Content>
-                                     </xml>';
-                echo $xml_response;
-                //写入数据库
-                $data = [
-                    'openid'    => $openid,
-                    'add_time'  => time(),
-                    'msg_type'  => 'voice',
-                    'media_id'  => $xml->MediaId,
-                    'format'    => $xml->Format,
-                    'msg_id'    => $xml->MsgId,
-                    'local_file_name'   => $file_name
-                ];
-
-                $m_id = WeixinMedia::insertGetId($data);
-                var_dump($m_id);
+                $this->dlVoice($xml->MediaId);
             }elseif($xml->MsgType=='video'){        //处理视频
-                $file_name=$this->dlVideo($xml->MediaId);
+               $this->dlVideo($xml->MediaId);
 
             }elseif($xml->MsgType=='event'){        //判断事件类型
 
@@ -232,7 +211,7 @@ class WeixinController extends Controller
         }else{      //保存失败
 
         }
-        return $file_name;
+
     }
 
     /**
@@ -258,7 +237,7 @@ class WeixinController extends Controller
         }else{      //保存失败
 
         }
-        return $file_name;
+
     }
 
 
