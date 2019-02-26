8b80fc6f53f92809092d3ab2af192b9524111e37
zty7777777 <724050978@qq.com>
2019-02-26 09:15:33 +0000

    微信支付

--- resources/views/weixin/pay.blade.php
+++ resources/views/weixin/pay.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="container" >
-    <h3>wechat支付:</h3>
-    <input type="hidden" id="code_curl" value="{{$code_curl}}">
+    <h3>wechat支付:{{$code_url}}</h3>
+    <input type="hidden" id="code_url" value="">
     <div id="code"></div>
 
 </div>
@@ -11,12 +11,12 @@
 <script src="{{URL::asset('/js/jquery-3.2.1.min.js')}}"></script>
 <script src="{{URL::asset('/js/jquery.qrcode.min.js')}}"></script>
 <script>
-    var code_curl=$("#code_curl").val();
+    var code_url=$("#code_curl").val();
 
     $("#code").qrcode({
         render: "canvas", //table方式
         width: 200, //宽度
         height:200, //高度
-        text: code_curl//任意内容
+        text: code_url//任意内容
     });
 </script>
