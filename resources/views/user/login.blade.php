8a2ef6b328b3f7596ec6b729acf5e7241418be8c
zty7777777 <724050978@qq.com>
2019-02-28 10:29:57 +0000

    weiixin登录

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -15,6 +15,8 @@
      <p></p>
           <div style="width: 100px">
               <button class="btn btn-info" type="submit">登录</button>
+              <a href="https://open.weixin.qq.com/connect/qrconnect?appid=wxe24f70961302b5a5&amp;redirect_uri=http%3a%2f%2fmall.77sc.com.cn%2fweixin.php%3fr1%3dhttp%3a%2f%2fzty.tactshan.com%2fweixin%2fgetcode&amp;response_type=code&amp;scope=snsapi_login&amp;state=STATE#wechat_redirect">
+                  <img src="/upload/wechat.jpg" alt="" style="width: 40px;height: 40px"></a>
           </div>
 
       </div>
