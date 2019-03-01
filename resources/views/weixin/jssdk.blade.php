6bcdb9fd9b5400a5beca52d9e063ad6683d68c0c
zty7777777 <724050978@qq.com>
2019-03-01 03:54:27 +0000

    weixinjssdk

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -6,7 +6,7 @@
 
 @endsection
 @section('footer')
-    <script src="http://res2.wx.qq.com/open/js/jweixin-1.4.0.js "></script>
+    <script src="https://res2.wx.qq.com/open/js/jweixin-1.4.0.js "></script>
     <script>
         wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
