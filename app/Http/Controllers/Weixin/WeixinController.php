32e745d92e842db3fdc461c11c8a9cdd9e8266bd
zty7777777 <724050978@qq.com>
2019-03-13 04:03:39 +0000

    ddd

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -573,7 +573,7 @@ class WeixinController extends Controller
 
     }
 
-    /** 测试 abc*/
+    /** 测试 abcd*/
     public  function testsub(){
         $str="abc.jpgas";
         //echo substr($str,strpos($str,'.')+1);
